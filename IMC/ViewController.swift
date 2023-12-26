d4121a1f63f1f37c24e87b018a7145846c24739e
Miqueias Pires <miqueiasousa011@gmail.com>
2023-12-26 11:25:29 -0300

    Initial Commit

---
+++ IMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Miqueias Pires on 26/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
